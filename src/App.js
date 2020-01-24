2b458567f4cab42d861beb7e8df6e7105845c84d
Emma Tilden <e.k.tilden@gmail.com>
2020-01-24 10:30:10 -0600

    some three variation and all basic explanations of preexisting setup

--- src/App.js
+++ src/App.js
@@ -5,12 +5,21 @@ class App extends React.Component {
   componentDidMount() {
 
     var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+
+    //1: field of view: the extend of the scene seen on the display at any given time
+    //2: aspect ratio: usually width/height to avoid distortion
+    //3 + 4: near + far clipping planes: where is too close or too far to see
+    var camera = new THREE.PerspectiveCamera( 45, window.innerWidth/window.innerHeight, 0.1, 1000 );
+
+    //sets how much of the screen this will take up. Can be used to reduce resolution
+    //WebGLRenderer is used for most current browsers. 
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
-    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+
+    
+    var geometry = new THREE.BoxGeometry( 1, 2, 3 );
+    var material = new THREE.MeshBasicMaterial( { color: 0x002a00 } );
     var cube = new THREE.Mesh( geometry, material );
     scene.add( cube );
     camera.position.z = 5;
