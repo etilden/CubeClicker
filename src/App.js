d7d6d48ecb2128304a5ed82585e1a54754713e47
Emma Tilden <e.k.tilden@gmail.com>
2020-01-26 22:27:54 -0600

    one cube kind of stays on screen and then rubs his head against the left side

--- src/App.js
+++ src/App.js
@@ -2,6 +2,19 @@ import React from 'react';
 import * as THREE from "three";
 
 class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state={
+      cubes: []
+    }
+
+    this.clickLocator = this.clickLocator.bind(this)
+    this.cubeIncludes = this.cubeIncludes.bind(this)
+    this.directionChanger = this.directionChanger.bind(this)
+    document.addEventListener('click', (event) => this.clickLocator(event))
+  }
+
   componentDidMount() {
 
     var scene = new THREE.Scene();
@@ -9,7 +22,7 @@ class App extends React.Component {
     //1: field of view (fov): the extend of the scene seen on the display at any given time
     //2: aspect ratio: usually width/height to avoid distortion. default is 2. 
     //3 + 4: near + far clipping planes: where is too close or too far to see
-    var camera = new THREE.PerspectiveCamera( 45, window.innerWidth/window.innerHeight, 0.1, 1000 );
+    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
     //(x, y, z)
     camera.position.set( 0, 0, 0 );
     camera.lookAt( 0, 0, 0 );
@@ -20,104 +33,115 @@ class App extends React.Component {
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
 
-    //resizing code-- doesn't work as far as I can see, but not sure I need it
-    // function resizeRendererToDisplaySize(renderer) {
-    //   const canvas = renderer.domElement;
-    //   const width = canvas.clientWidth;
-    //   const height = canvas.clientHeight;
-    //   const needResize = canvas.width !== width || canvas.height !== height;
-    //   if (needResize) {
-    //     renderer.setSize(width, height, false);
-    //   }
-    //   return needResize;
-    // }
+    let geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    camera.position.z = 5;
 
-    // resizeRendererToDisplaySize(renderer)
+    //add light to the scene
+    const color = 0xFFFFFF;
+    const intensity = 1;
+    const light = new THREE.DirectionalLight(color, intensity);
+    light.position.set(-1, 2, 4);
+    scene.add(light);
+    
+    //make the cubes
+    function makeInstance(geometry, color, x, y, name) {
+      const material = new THREE.MeshPhongMaterial({color});
+      const cube = new THREE.Mesh(geometry, material);
+      scene.add(cube);
+      cube.position.x = x; 
+      cube.position.y = y;
+      cube.name = name;
+      return cube;  
+    }
 
-  //single cube
-    // let geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    // let material = new THREE.MeshPhongMaterial( { color: 0x117E2C } );
-    // let cube = new THREE.Mesh( geometry, material );
-    // scene.add( cube );
-    // camera.position.z = 5;
+    const cubeA = makeInstance(geometry, 0x44aa88, 0, 0, 'greenish');
+    const cubeB = makeInstance(geometry, 0x8844aa, -2, -2, 'purple');
+    const cubeC = makeInstance(geometry, 0xaa8844,  1, 1, 'yellow');
+    
+    const cubes = [
+      cubeA,
+      cubeB,
+      cubeC,
+    ];
 
-    // const color = 0xFFFFFF;
-    // const intensity = 1;
-    // const light = new THREE.DirectionalLight(color, intensity);
-    // light.position.set(-1, 2, 4);
-    // scene.add(light);
+    this.setState({cubes: [...cubes]})
+    
+    // spin
+    let animate = function () {
+      requestAnimationFrame( animate );
+      cubes.forEach((cube, i) => {
+        let additional = (i+1)*0.01
+        cube.rotation.x += additional;
+        cube.rotation.y += additional;
 
-    //   //add light to the scene
-    // let animate = function () {
-    //   requestAnimationFrame( animate );
-    //   cube.rotation.x += 0.01;
-    //   cube.rotation.y += 0.01;
-    //   renderer.render( scene, camera );
-    // };
-    // animate();
+      })
+      renderer.render( scene, camera );
+    };
+    animate();
+    
 
-  //multiple cubes
-      let geometry = new THREE.BoxGeometry( 1, 1, 1 );
-      camera.position.z = 5;
-      
-      function makeInstance(geometry, color, x) {
-        const material = new THREE.MeshPhongMaterial({color});
-        const cube = new THREE.Mesh(geometry, material);
-        scene.add(cube);
-        cube.position.x = x; 
-        return cube;  
+  //travel
+  let travel = function () {
+    requestAnimationFrame( travel );
+    let xdifferentiation = 0.01;
+    let ydifferentiation = 0.01;
+    cubes.forEach((cube) => {
+      if (cube.position.x <= 9 && cube.position.x >= -9) {
+        xdifferentiation*=(-1);
+        ydifferentiation*=(-1);
+        // console.log(xdifferentiation)
       }
+      if (cube.position.y <= 5 && cube.position.y >= -5) {
+        ydifferentiation*=(-1);
+      }
+      cube.position.x += xdifferentiation;
+      cube.position.y -= ydifferentiation;
+      // console.log('x', cube.position.x, 'y', cube.position.y)
+    })
+    renderer.render( scene, camera );
+  };
+    
+    travel(); 
 
-      const cubes = [
-        makeInstance(geometry, 0x44aa88,  0),
-        makeInstance(geometry, 0x8844aa, -2),
-        makeInstance(geometry, 0xaa8844,  2),
-      ];
+    }
 
-      //add light to the scene
-      const color = 0xFFFFFF;
-      const intensity = 1;
-      const light = new THREE.DirectionalLight(color, intensity);
-      light.position.set(-1, 2, 4);
-      scene.add(light);
+      cubeIncludes = function(cube, clickX, clickY) {
+        let cubeX = {
+          xPosition: cube.position.x * 80 + 720,
+          xMin: cube.position.x * 80 + 651,
+          xMax: cube.position.x * 80 + 789,
+        };
+      
+        let cubeY = {
+          yPosition: cube.position.y * 87.7 + (window.innerHeight/2),
+          yMin: -1*cube.position.y * 87.7 + (window.innerHeight/2) - 69,
+          yMax: -1*cube.position.y * 87.7 + (window.innerHeight/2) + 69,
+        };
 
-
-      let animate = function () {
-        requestAnimationFrame( animate );
-        cubes.forEach((cube, i) => {
-          let additional = (i+1)*0.01
-          cube.rotation.x += additional;
-          cube.rotation.y += additional;
+        // console.log('cpx', cube.position.x, 'clickX', clickX , 'cubeX.xMin', cubeX.xMin, 'cubeX.xMax', cubeX.xMax) 
+        // console.log('cpy', cube.position.y, "clickY", clickY, "cubeY.yMin", cubeY.yMin, 'cubeY.yMax', cubeY.yMax)
+        // console.log("clickY", clickY, "cubeY.yMin", cubeY.yMin, 'cubeY.yMax', cubeY.yMax, 'cpy', cube.position.y)
+      
+        if (
+          clickX >= cubeX.xMin &&
+          clickX <= cubeX.xMax &&
+          clickY >= cubeY.yMin &&
+          clickY <= cubeY.yMax
+        ) {
           return cube;
-        })
-        renderer.render( scene, camera );
+        }
       };
-      animate();
 
-  //line
-    // var material = new THREE.LineBasicMaterial( { color: 0xFF0000 } );
-    // var geometry = new THREE.Geometry();
-    // geometry.vertices.push(new THREE.Vector3(-10, 0, 0) );
-    // geometry.vertices.push(new THREE.Vector3(0, 10, 10) );
-    // geometry.vertices.push(new THREE.Vector3(10, 0, 0) );
-    // geometry.vertices.push(new THREE.Vector3(10, 0, 10) );
-    // geometry.vertices.push(new THREE.Vector3(10, 10, 10) );
-    // geometry.vertices.push(new THREE.Vector3(10, 10, 0) );
+    directionChanger = (cubeArr) => {
+        console.log(cubeArr[0])
+      }
+    clickLocator = (event) => {
+      let clickedCubes= this.state.cubes.filter(cube => this.cubeIncludes(cube, event.pageX, event.pageY));
+      console.log('ouch', clickedCubes)
+      this.directionChanger(clickedCubes)
+    }
 
-    // var line = new THREE.Line( geometry, material );
-    // scene.add( line );
-    // camera.position.z = 5;
-    // var animate = function () {
-    //   requestAnimationFrame( animate );
-    //   line.rotation.x += 0.01;
-    //   line.rotation.y += 0.01;
-    //   renderer.render( scene, camera );
-    // };
-    // animate();
 
-  //loading 3D models (the below is not for 3d models, it is from the cubes workshop. Just wanted to have a starting place)
-
-  }
 
   render() {
     return (
