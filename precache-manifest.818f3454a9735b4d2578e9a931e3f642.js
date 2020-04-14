2ff43dcce35585d88c573d0a75a3ff5cc05c56b8
Emma Tilden <e.k.tilden@gmail.com>
2020-04-13 21:37:37 -0500

    Updates

---
+++ precache-manifest.818f3454a9735b4d2578e9a931e3f642.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "026b5390d504093e8e7f5c8cb775bca7",
+    "url": "/cube-clicker/index.html"
+  },
+  {
+    "revision": "2fcd6bad9e2f0b94911f",
+    "url": "/cube-clicker/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4e679845e4b468419cd2",
+    "url": "/cube-clicker/static/js/2.fb54ca3c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/cube-clicker/static/js/2.fb54ca3c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2fcd6bad9e2f0b94911f",
+    "url": "/cube-clicker/static/js/main.8698afe9.chunk.js"
+  },
+  {
+    "revision": "5dd8545f92916d8750dd",
+    "url": "/cube-clicker/static/js/runtime-main.0dc3d548.js"
+  }
+]);
